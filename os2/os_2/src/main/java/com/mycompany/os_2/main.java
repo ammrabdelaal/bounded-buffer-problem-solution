04365359846e5d53063fdc49745ba2f063a98e10
zika4665 <96925217+zika4665@users.noreply.github.com>
2022-12-19 08:59:17 +0200

    Update main.java

--- os2/os_2/src/main/java/com/mycompany/os_2/main.java
+++ os2/os_2/src/main/java/com/mycompany/os_2/main.java
@@ -6,17 +6,20 @@ import com.mycompany.os_2.consumer;
 import com.mycompany.os_2.data_Q;
 
 public class main {
+    public static void main (String args[]) throws InterruptedException{
+        data_Q dataQueue = new data_Q(4);
+        
+                producer producing_line = new producer(dataQueue);
+                for(int i = 0; i < 2; i++) {
+                    Thread producerThread = new Thread(producing_line);
+                    producerThread.start();
+                }
     
-    data_Q dataQueue = new data_Q(8);
-    
-    producer Producer = new producer(dataQueue);
-    Thread producerThread = new Thread(Producer);
-    
-    consumer Consumer = new consumer(dataQueue);
-    Thread consumerThread = new Thread(Consumer);
-    
-    producerThread.start();
-    consumerThread.start();
-    
-
+                consumer targeted_product = new consumer(dataQueue);
+                for(int i = 0; i < 4; i++) {
+                    Thread consumerThread = new Thread(targeted_product);
+                    consumerThread.start();
+                }
+        
+    }
 }
